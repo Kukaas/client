d7ae0f964340dfce64498f85784368607bac6659
Kukaass <chestermaligaso29@gmail.com>
2024-06-16 13:54:16 +0800

    created the routes for each pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+
 const App = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
